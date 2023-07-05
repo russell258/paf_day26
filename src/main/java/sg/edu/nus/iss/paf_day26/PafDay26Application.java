809753010d1198bd1b98b3c99156479e697f8b0b
russell258 <duckie258@gmail.com>
2023-07-05 13:28:57 +0800

    set up

---
+++ src/main/java/sg/edu/nus/iss/paf_day26/PafDay26Application.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.paf_day26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PafDay26Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PafDay26Application.class, args);
+	}
+
+}
